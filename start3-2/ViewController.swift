8bbef1dc71a3bbc7a6d330f7104dea8e13e82e88
babbayo <na.babbayo@gmail.com>
2019-11-11 23:39:56 +0900

    Initial Commit

---
+++ start3-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  start3-2
+//
+//  Created by MacBook on 11/11/2019.
+//  Copyright © 2019 yaco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
